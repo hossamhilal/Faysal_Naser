1b55677618c6ff573cccc32143cc76262a34a503
Hossam Hilal <46751584+hossamhilal@users.noreply.github.com>
2021-08-22 12:23:15 +0200

    Upload Files

---
+++ js/custom.js
@@ -0,0 +1,70 @@
+/*global $ */
+(function($) {
+    "use strict";
+
+    // OPEN SIDE  MENU 
+    $('.menuBtn').on('click', function(){
+        $('.mainMenu').toggleClass('opend');
+        $('.navOverlay').addClass('opend');  
+        setTimeout(function(){
+            $('body').addClass('stopScroll');
+        }, 200); 
+    });
+
+    // CLOSE SIDE MENU 
+    $('.navOverlay').on('click', function(){
+        $(this).removeClass('opend');
+        $('.mainMenu').removeClass('opend');  
+        $('body').removeClass('stopScroll');  
+    });
+
+    // Check if Rtl 
+    var rtlVal = true ;   
+    $('body').hasClass('en') ? rtlVal = false : rtlVal = true;
+
+    // Header OWL 
+    $('.owlHeader').owlCarousel({
+        rtl: rtlVal,
+        margin: 0,
+        autoplay: true,
+        loop: true,
+        nav: false,
+        dots: false,
+        autoplaySpeed : 2000,
+        autoplayTimeout : 2000,
+        smartSpeed: 2000 ,
+        navText: ["<i class='icofont-long-arrow-right'></i>", "<i class='icofont-long-arrow-left'></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    // // INPUT FOCUS ANIMATION 
+    // $('.inputField input').focus(function(){
+    //     $(this).parent('.inputField').addClass('focused');
+    // });
+
+    // $('.inputField input').each(function() { 
+    //     if ($(this).val() != "") {
+    //         $(this).parent('.inputField').addClass('focused');   
+    //     }
+    // });
+
+    // $('.inputField input').focusout(function(){
+    //     if($(this).val() === "")
+    //     $(this).parent('.inputField').removeClass('focused');
+    // });
+
+    // iniat WOW Js
+    new WOW().init();
+   
+})(jQuery);
+
